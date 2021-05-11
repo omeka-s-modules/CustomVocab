7fc712a7a75f4f4bfc4ccf632564d46a69b1269b
Jim Safley <jimsafley@gmail.com>
2021-05-11 00:25:32 -0400

    Begin implementing URI custom vocab type

--- asset/js/custom-vocab.js
+++ asset/js/custom-vocab.js
@@ -7,3 +7,12 @@ $(document).on('o:prepare-value', function(e, type, value) {
         });
     }
 });
+
+$(document).on('change', 'select.custom-vocab-uri', function(e) {
+    const thisSelect = $(this);
+    const label = thisSelect.children(':selected').data('label');
+    const labelInput = $(`<input type="hidden" data-value-key="o:label">`);
+    labelInput.attr('value', label);
+    thisSelect.after(labelInput);
+});
+
--- src/DataType/CustomVocab.php
+++ src/DataType/CustomVocab.php
@@ -61,16 +61,42 @@ class CustomVocab extends AbstractDataType
                 );
             }
             $select->setAttribute('data-value-key', 'value_resource_id')
+                ->setAttribute('class', 'terms to-require')
                 ->setEmptyOption($view->translate('Select item below'));
+        } else if ($this->vocab->uris()) {
+            $uris = array_map('trim', preg_split("/\r\n|\n|\r/", $this->vocab->uris()));
+            $valueOptions = [];
+            foreach ($uris as $uri) {
+                if (preg_match('/^(\S+) (.+)$/', $uri, $matches)) {
+                    $uri = $matches[1];
+                    $label = $matches[2];
+                    $valueOptions[] = [
+                        'value' => $uri,
+                        'label' => sprintf($view->translate('%s <%s>'), $label, $uri),
+                        'attributes' => [
+                            'data-label' => $label,
+                        ],
+                    ];
+                } elseif (preg_match('/^(.+)/', $uri, $matches)) {
+                    $uri = $matches[1];
+                    $valueOptions[] = [
+                        'value' => $uri,
+                        'label' => $uri,
+                    ];
+                }
+            }
+            $select->setAttribute('data-value-key', '@id')
+                ->setAttribute('class', 'terms to-require custom-vocab-uri')
+                ->setEmptyOption($view->translate('Select URI below'));
         } else {
             // Normalize vocab terms for use in a select element.
             $terms = array_map('trim', preg_split("/\r\n|\n|\r/", $this->vocab->terms()));
             $valueOptions = array_combine($terms, $terms);
             $select->setAttribute('data-value-key', '@value')
+                ->setAttribute('class', 'terms to-require')
                 ->setEmptyOption($view->translate('Select term below'));
         }
-        $select->setValueOptions($valueOptions)
-            ->setAttribute('class', 'terms to-require');
+        $select->setValueOptions($valueOptions);
 
         return $view->formSelect($select);
     }
@@ -79,6 +105,11 @@ class CustomVocab extends AbstractDataType
     {
         if (isset($valueObject['value_resource_id'])
             && is_numeric($valueObject['value_resource_id'])
+        ) {
+            return true;
+        } elseif ($valueObject['@id']
+            && is_string($valueObject['@id'])
+            && '' !== trim($valueObject['@id'])
         ) {
             return true;
         } elseif (isset($valueObject['@value'])
@@ -96,6 +127,12 @@ class CustomVocab extends AbstractDataType
             && is_numeric($valueObject['value_resource_id'])
         ) {
             $dataTypeName = 'resource:item';
+        } elseif ($valueObject['@id']
+            && is_string($valueObject['@id'])
+            && '' !== trim($valueObject['@id'])
+        ) {
+            $dataTypeName = 'uri';
+            $valueObject['@language'] = $this->vocab->lang();
         } elseif (isset($valueObject['@value'])
             && is_string($valueObject['@value'])
             && '' !== trim($valueObject['@value'])
@@ -115,6 +152,14 @@ class CustomVocab extends AbstractDataType
         if ($valueResource) {
             return $valueResource->linkPretty();
         }
+        if ($value->uri()) {
+            $uri = $value->uri();
+            $uriLabel = $value->value();
+            if (!$uriLabel) {
+                $uriLabel = $uri;
+            }
+            return $view->hyperlink($uriLabel, $uri, ['target' => '_blank']);
+        }
         return nl2br($view->escapeHtml($value->value()));
     }
 
@@ -123,6 +168,13 @@ class CustomVocab extends AbstractDataType
         $valueResource = $value->valueResource();
         if ($valueResource) {
             return $valueResource->valueRepresentation();
+        }
+        if ($value->uri()) {
+            $jsonLd = ['@id' => $value->uri()];
+            if ($value->value()) {
+                $jsonLd['o:label'] = $value->value();
+            }
+            return $jsonLd;
         }
         $jsonLd = ['@value' => $value->value()];
         if ($value->lang()) {
